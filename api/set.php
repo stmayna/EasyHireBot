a178f6af9537f7ee39cefe2e199d8e60661ccb02
stmayna <stmayna@gmail.com>
2022-12-20 13:57:15 +0700

    initiate set.php

--- api/set.php
+++ api/set.php
@@ -4,7 +4,8 @@ require __DIR__ . '/vendor/autoload.php';
 
 $bot_api_key  = '5961232438:AAExGsqkhQarNtIlvLPExiDJ2XoV6EtjP34';
 $bot_username = 'easy_hire_bot';
-$hook_url     = 'https://easy-hire-bot.vercel.app/api/index.php';
+$hook_url     = 'https://easy-hire-bot.vercel.app/api/hook.php';
+// $result = $telegram->setWebhook($hook_url, ['certificate' => '/path/to/certificate']);
 
 try {
     // Create Telegram API object
